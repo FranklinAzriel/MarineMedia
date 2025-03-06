41603c8df7519d78de1ddb795e070a02101f8dd3
FranklinAzriel <franklinazriel6942@gmail.com>
2025-03-06 19:17:27 +0700

    feat: Add basic SettingsPage scaffold
    This commit adds a basic scaffold for the SettingsPage. Currently, it only displays an AppBar with the title "Settings". Further UI elements and functionality will be added in subsequent commits.

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marinemedia/pages/settingspage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +13,29 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Marine Media")),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              child: Text("Marine Media"),
+            ),
+
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsPage(),
+                    ),
+                  );
+                },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
